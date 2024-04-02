ce05bfe431cca346e3e6e8b1560096300a5f1738
Abe-Tiz <106752541+Abe-Tiz@users.noreply.github.com>
2024-04-02 14:28:26 +0300

    add router

---
+++ client/src/layout/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import App from '../App';
+
+const Main = () => {
+  return (
+    <div className='border-2'>
+      <h1>hello world</h1>
+    </div>
+  )
+}
+
+export default Main
